6ab38a6324a9b5b237be8842c9b400ff581030f9
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-12-06 23:14:33 -0300

    att

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,7 +5,7 @@ gcc darwin.c -o darwin.o
 curl -L https://github.com/OUIsolutions/LuaCEmbed/releases/download/v0.778/LuaCEmbed.h -o dependencies/LuaCEmbed.h
 
 
-curl -L https://github.com/OUIsolutions/SilverChain/releases/download/v0.07/CSilverChainApiNoDependenciesIncluded.h -o  dependencies/CSilverChainApiNoDependenciesIncluded.h
+curl -L https://github.com/OUIsolutions/SilverChain/releases/download/v0.08/CSilverChainApiNoDependenciesIncluded.h -o  dependencies/CSilverChainApiNoDependenciesIncluded.h
 curl -L https://github.com/OUIsolutions/CTextEngine/releases/download/v2.002/CTextEngine.h -o dependencies/CTextEngine.h
 curl -L https://github.com/OUIsolutions/DoTheWorld/releases/download/v7.005/doTheWorld.h -o dependencies/doTheWorld.h
 curl -L https://github.com/OUIsolutions/C-Cli-Entry/releases/download/0.001/CliEntry.h -o  dependencies/CliEntry.h
