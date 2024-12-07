ad804209ea7eff221adea24d89a0db573a3d7908
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-12-06 21:32:23 -0300

    att

--- citerop.c
+++ citerop.c
@@ -4,67 +4,25 @@
 #include "dependencies/CSilverChainApiNoDependenciesIncluded.h"
 
 
-short private_lua_c_amalgamator_generator_callback(const char *filename,const char *include_name,void *args){
-    LuaCEmbed *args_formmated = (LuaCEmbed*)args;
-    LuaCEmbedTable *args_data = LuaCembed_new_anonymous_table(args_formmated);
-    LuaCEmbedTable_append_string(args_data, filename);
-    LuaCEmbedTable_append_string(args_data, include_name);
-    LuaCEmbedTable *return_args = LuaCEmbed_run_args_lambda(args, 3, args_data,1);
-    long result  = LuaCEmbedTable_get_long_by_index(return_args,0);
-    if(LuaCEmbed_has_errors(args_formmated)){
-          return CAMALGAMATOR_UNEXPECTED_ERROR;
-    }
-    return result;
-
+LuaCEmbedResponse * private_silver_chain_lua_new_SiverChainStringArray(LuaCEmbed *args){
+    SilverChainStringArray *value = newSilverChainStringArray();
+    return LuaCEmbed_send_long((long)value);
 }
 
-LuaCEmbedResponse *private_lua_c_amalgamator_generate_amalgamation_simple(LuaCEmbed *args){
-
-    CAmalgamatorErrorOrContent *error_or_content =CAmalgamator_generate_amalgamation_simple(
-        LuaCEmbed_get_str_arg(args, 0),
-        LuaCEmbed_get_long_arg(args,1),
-        LuaCEmbed_get_long_arg(args,2)
-    );
-    LuaCEmbedResponse *response = NULL;
-
-    if(error_or_content->error){
-        response = LuaCEmbed_send_error(error_or_content->error_msg);
-    }
-    else{
-        response = LuaCEmbed_send_str(error_or_content->content);
-    }
-
-    CAmalgamatorErrorOrString_free(error_or_content);
-    return response;
+LuaCEmbedResponse * private_silver_chain_lua_free_SiverChainStringArray(LuaCEmbed *args){
+    SilverChainStringArray *value = (SilverChainStringArray*)LuaCEmbed_get_long_arg(args,0);
+    SilverChainStringArray_free(value);
+    return NULL;
+}
+LuaCEmbedResponse * private_silver_chain_lua_apend_SiverChainStringArray(LuaCEmbed *args){
+    SilverChainStringArray *value = (SilverChainStringArray*)LuaCEmbed_get_long_arg(args,0);
+    char *str  = LuaCEmbed_get_str_arg(args, 1);
+    SilverChainStringArray_append(value, str);
+    return NULL;
 }
 
-LuaCEmbedResponse *private_lua_c_amalgamator_generate_amalgamation_complex(LuaCEmbed *args){
-    CAmalgamatorErrorOrContent *error_or_content =CAmalgamator_generate_amalgamation(
-        LuaCEmbed_get_str_arg(args, 0),
-        LuaCEmbed_get_long_arg(args,1),
-        LuaCEmbed_get_long_arg(args,2),
-        private_lua_c_amalgamator_generator_callback,
-        (void*)args
-    );
-    LuaCEmbedResponse *response = NULL;
 
-    if(LuaCEmbed_has_errors(args)){
-          CAmalgamatorErrorOrString_free(error_or_content);
-          return LuaCEmbed_send_error(LuaCEmbed_get_error_message(args));
-    }
-
-    if(error_or_content->error){
-        response = LuaCEmbed_send_error(error_or_content->error_msg);
-    }
-    else{
-        response = LuaCEmbed_send_str(error_or_content->content);
-    }
-
-    CAmalgamatorErrorOrString_free(error_or_content);
-    return response;
-}
-
-int luaopen_private_lua_c_silverchain_cinterop(lua_State *state){
+int luaopen_private_silverchain_cinterop(lua_State *state){
     //functions will be only assescible by the required reciver
     LuaCEmbed * l = newLuaCEmbedLib(state);
     LuaCEmbed_set_long_lib_prop(l,"CAMALGAMATOR_UNEXPECTED_ERROR",CAMALGAMATOR_UNEXPECTED_ERROR);
--- public/memory.lua
+++
@@ -1,2 +0,0 @@
-public_lua_c_amalgamator.ONE_BYTE = 1024
-public_lua_c_amalgamator.ONE_MB = public_lua_c_amalgamator.ONE_BYTE * 1000
